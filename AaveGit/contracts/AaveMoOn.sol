edc8e39f8ad22a37c6f0f1536b0cf39eddf05dcd
Toolback <93286025+Toolback@users.noreply.github.com>
2022-04-04 12:31:52 +0200

    Update AaveMoOn.sol

--- AaveGit/contracts/AaveMoOn.sol
+++ AaveGit/contracts/AaveMoOn.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.8.10;
 pragma abicoder v2;
 
+// Polygon Mainnet Fork Version
+// run npx hardhat test 
+
 
 import '@aave/core-v3/contracts/interfaces/IPool.sol';
 import '@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol';
