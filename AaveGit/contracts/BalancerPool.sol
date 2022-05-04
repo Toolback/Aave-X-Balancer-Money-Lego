fa0022b0ed5020c4427a7f885a3c5bc571f6e547
Toolback <93286025+Toolback@users.noreply.github.com>
2022-05-04 15:36:08 +0200

    Update BalancerPool.sol

--- AaveGit/contracts/BalancerPool.sol
+++ AaveGit/contracts/BalancerPool.sol
@@ -5,42 +5,36 @@ import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
 // import "@balancer-labs/v2-vault/contracts/interfaces/IBasePool.sol";
 
 contract BalancerPool {
-    IVault internal immutable Vault;
-    
-    // IBasePool public Pool;
-
-    constructor() {
-        Vault = IVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-    }
 
     function joinPool(
+        address vaultAddress,
         bytes32 poolId,
         address sender,
         address recipient,
         IVault.JoinPoolRequest memory request
     ) public payable {
-        Vault.joinPool(poolId, sender, recipient, request);
+
+        IVault(vaultAddress)
+        .joinPool(poolId, sender, recipient, request);
     }
-
-    function GPT(bytes32 _poolId) public view returns (
-        IERC20[] memory tokens, 
-        uint256[] memory balances,
-        uint256 lastChangeBlock){
-        Vault.getPoolTokens(_poolId);
-    }
-
-    // function getPoolId(address _pool) external view returns (bytes32) {
-    //     Pool = IBasePool(_pool);
-    //     return Pool.getPoolId();
-    // }
-
 
     function exitPool(
+        address _vaultAddress,
         bytes32 poolId,
         address sender,
         address payable recipient,
         IVault.ExitPoolRequest memory request
     ) public {
-        Vault.exitPool(poolId, sender, recipient, request);
+        IVault(_vaultAddress)
+        .exitPool(poolId, sender, recipient, request);
+    }
+
+
+    function GetPoolTokens(bytes32 _poolId, address _vaultAddress) public view returns (
+        IERC20[] memory tokens, 
+        uint256[] memory balances,
+        uint256 lastChangeBlock){
+        IVault(_vaultAddress)
+        .getPoolTokens(_poolId);
     }
 }
