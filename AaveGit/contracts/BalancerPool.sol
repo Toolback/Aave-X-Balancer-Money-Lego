38a9f68c498321f0b5af3a33927b0dd5118189d4
Toolback <93286025+Toolback@users.noreply.github.com>
2022-05-06 12:47:06 +0200

    Update BalancerPool.sol

--- AaveGit/contracts/BalancerPool.sol
+++ AaveGit/contracts/BalancerPool.sol
@@ -3,8 +3,10 @@ pragma experimental ABIEncoderV2;
 
 import "@balancer-labs/v2-vault/contracts/interfaces/IVault.sol";
 // import "@balancer-labs/v2-vault/contracts/interfaces/IBasePool.sol";
+// import "@balancer-labs/v2-vault/contracts/interfaces/IFlashLoanRecipient.sol";
 
-contract BalancerPool {
+contract BalancerPool is IFlashLoanRecipient  {
+    IVault public flashVault = "0x0297e37f1873d2dab4487aa67cd56b58e2f27875";
 
     function joinPool(
         address vaultAddress,
@@ -28,6 +30,38 @@ contract BalancerPool {
         IVault(_vaultAddress)
         .exitPool(poolId, sender, recipient, request);
     }
+
+    function swapTokens(
+        address _vaultAddress,
+        IVault.SingleSwap memory singleSwap,
+        IVault.FundManagement memory funds,
+        uint256 limit,
+        uint256 deadline) public{
+        IVault(_vaultAddress)
+            .swap(singleSwap,
+                funds,
+                limit,
+                deadline);
+    }
+
+    // function makeFlashLoan(
+    //     IERC20[] memory tokens,
+    //     uint256[] memory amounts,
+    //     bytes memory userData
+    // ) external {
+    //   flashVault
+    //   .flashLoan(this, tokens, amounts, userData);
+    // }
+
+    // function receiveFlashLoan(
+    //     IERC20[] memory tokens,
+    //     uint256[] memory amounts,
+    //     uint256[] memory feeAmounts,
+    //     bytes memory userData
+    // ) external override {
+    //     require(msg.sender == flashVault);
+        
+    // }
 
 
     function GetPoolTokens(bytes32 _poolId, address _vaultAddress) public view returns (
